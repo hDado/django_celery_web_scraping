9d8c3390a7540cd3ea9fe2d273826cf72f89c54a
Hamza AZZOUZ <azzouzhamza13@gmail.com>
2024-04-03 00:08:19 +0100

    initial config django celery

---
+++ src/movies/tasks.py
@@ -0,0 +1,20 @@
+from celery import shared_task
+
+
+@shared_task
+def add(x,y):
+
+    return x + y
+
+
+@shared_task(name="multiply_two_numbers")
+def mul(x, y):
+    # Celery recognizes this as the `multiple_two_numbers` task
+    total = x * (y * random.randint(3, 100))
+    return total
+
+
+@shared_task(name="sum_list_numbers")
+def xsum(numbers):
+    # Celery recognizes this as the `sum_list_numbers` task
+    return sum(numbers)    
